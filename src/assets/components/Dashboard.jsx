ba37a70a70ac5f8dde5c9e7371e1d72be3ad5db3
Edward Catapan <47245860+xdwardd@users.noreply.github.com>
2025-07-02 10:31:01 +0800

    Update Dashboard.jsx

--- src/assets/components/Dashboard.jsx
+++ src/assets/components/Dashboard.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Dashboard = ({name}) => {
   return (
-    <div className='text-2xl text-blue-500'> Welcome Back! {name}</div>
+    <div className="h-screen flex items-center justify-center">
+      <div className="text-2xl text-blue-500 font-bold text-center">
+        {" "}
+        Welcome Back! <span className='text-red-500'>{name}</span>
+      </div>
+    </div>
   )
 }
 
